e8240f0ab09f7f0b3dec7dfefffe582263f4d590
Manuel Parra <manuel.parra1@gmail.com>
2025-09-21 15:02:11 -0500

    sops key management

--- flake.nix
+++ flake.nix
@@ -1,29 +1,37 @@
 {
   description = "NixOS + Home Manager for dusts";
-inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-  nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  home-manager.url = "github:nix-community/home-manager/release-24.05";
-  home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-  dotfiles = { url = "github:manuelparra1/dotfiles"; flake = false; };
-};
-
-outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, dotfiles, ... }:
-let
-  system = "x86_64-linux";
-  pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-  pkgsUnstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
-in {
-  nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-    inherit system;
-    modules = [
-      ./hosts/nixos.nix
-      home-manager.nixosModules.home-manager
-      { home-manager.users.dusts = import ./home/dusts.nix { inherit pkgs pkgsUnstable dotfiles; }; }
-    ];
+    sops-nix.url = "github:Mic92/sops-nix";
   };
-};
+
+  outputs = { self, nixpkgs, home-manager, sops-nix, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/default.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # Import your HM module from the dotfiles repo:
+            # (adjust the relative path to your dotfiles)
+            home-manager.users.dusts = import ../dotfiles/home/dusts.nix;
+
+            # Make sops-nix available *inside* your HM module:
+            home-manager.extraSpecialArgs = { inherit sops-nix; };
+          }
+        ];
+      };
+    };
 
 }
