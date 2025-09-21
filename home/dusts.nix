7f836bfae18befc09d0e1dd074a006c6e7f56cdd
Manuel Parra <manuel.parra1@gmail.com>
2025-09-21 19:24:52 +0000

    tree

--- home/dusts.nix
+++ home/dusts.nix
@@ -28,7 +28,7 @@
     wl-clipboard
   
     # CLI/dev tools
-    neovim git ripgrep fd starship zsh fzf zoxide bat jq unzip
+    neovim git ripgrep fd starship zsh fzf zoxide bat jq unzip tree
 
     # ADD YOUR FONTS FROM NIXPKGS HERE
     (nerdfonts.override { fonts = [ "JetBrainsMono" "FiraCode" "0xProto" ]; })
